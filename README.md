ab41c8344d71414ece46eb8b149ee811410baebd
jnaraja <j2naraja@gmail.com>
2019-03-02 11:40:07 -0800

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CMPE-142-HW1 12345
+# comment
---
+++ test.c
@@ -0,0 +1,23 @@
+#include <err.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv [])
+{
+	char *line = NULL;
+	size_t linesize = 0;
+	ssize_t linelen;
+	while((linelen = getline(&line, &linesize, stdin)) != -1) {
+		if (strncmp ("exit", line, 4) == 0){
+			exit(0);
+		}
+		
+		fwrite(line, linelen, 1, stdout);
+	}
+	
+	free(line);
+	if(ferror(stdin)) {
+		err(1, "getline");
+	}
+}
